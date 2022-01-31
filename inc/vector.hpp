3aadca8739f3296146b744848a0d196dc45b5b59
adelille <adelille@student.42.fr>
2022-01-31 11:43:16 +0100

    defining function

--- inc/vector.hpp
+++ inc/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 12:14:18 by adelille          #+#    #+#             */
-/*   Updated: 2022/01/31 11:32:44 by adelille         ###   ########.fr       */
+/*   Updated: 2022/01/31 11:43:05 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,40 @@ class vector
 		vector(void);
 		vector(const vector &src);
 		virtual ~vector(void);
-		
+
 		vector	&operator=(const vector &rhs);
+
+		// element access
+
+		// iterators
+		iterator				begin();
+		const_iterator			begin() const;
+		iterator				end();
+		const_iterator			end() const;
+		reverse_iterator		rbegin();
+		const_reverse_iterator	rbegin() const;
+		reverse_iterator		rend();
+		const_reverse_iterator	rend() const;
+
+		// capacity
+
+		// modifiers
 
 	private: //
 };
 
-std::ostream	&operator<<(std::ostream &o, vector const &src);
+//std::ostream	&operator<<(std::ostream &o, vector const &src);
+
+template< class T, class Alloc >
+bool	operator==(const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
+bool	operator!=(const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
+bool	operator<(const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
+bool	operator<=(const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
+bool	operator>(const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
+bool	operator>=(const std::vector<T,Alloc>& lhs, const std::vector<T,Alloc>& rhs);
+
+// std::swap
+template< class T, class Alloc >
+void	swap( std::vector<T,Alloc>& lhs, std::vector<T,Alloc>& rhs );
 
 #endif
