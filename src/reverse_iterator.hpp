23a0654cfae78373957b1b52008c7200aa5b893a
Alexdelia <alexandre.delille.57@gmail.com>
2022-02-02 18:51:03 +0100

    coding reverse_iterator

--- src/reverse_iterator.hpp
+++ src/reverse_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 18:25:54 by adelille          #+#    #+#             */
-/*   Updated: 2022/02/02 18:32:13 by adelille         ###   ########.fr       */
+/*   Updated: 2022/02/02 18:48:52 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define REVERSE_ITERATOR_HPP
 
 # include "iterator_traits.hpp"
+# include "random_access_iterator.hpp"
 
 namespace ft
 {
@@ -27,6 +28,24 @@ namespace ft
 				typedef typename iterator_traits<Iterator>::pointer				pointer;
 				typedef typename iterator_traits<Iterator>::reference			reference;
 				typedef typename iterator_traits<Iterator>::iterator_category	iterator_category;
+
+				reverse_iterator() : _ptr(Iterator()) {};
+				explicit reverse_iterator (iterator_type it) : _ptr(it) {};
+				reverse_iterator (const reverse_iterator<Iterator>& rev_it) : _ptr(rev_it._ptr) {};
+
+				reverse_iterator	&operator=(const reverse_iterator& it)
+				{
+					_ptr = it._ptr;
+					return (*this);
+				}
+
+				operator	reverse_iterator<random_access_iterator<value_type const> >() const
+				{return (reverse_iterator<random_access_iterator<value_type const> >(_ptr));}
+				operator	reverse_iterator<random_access_iterator<value_type> >()
+				{return (reverse_iterator<random_access_iterator<value_type> >(_ptr));}
+
+			private:
+				iterator_type	_ptr;
 		};
 }
 
