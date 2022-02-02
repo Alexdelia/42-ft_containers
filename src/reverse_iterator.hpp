73e778c5132ee421c614ce2be1048a17a034852f
Alexdelia <alexandre.delille.57@gmail.com>
2022-02-02 18:42:31 +0100

    added iterator_traits.hpp

--- src/reverse_iterator.hpp
+++ src/reverse_iterator.hpp
@@ -6,12 +6,14 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 18:25:54 by adelille          #+#    #+#             */
-/*   Updated: 2022/02/02 18:29:44 by adelille         ###   ########.fr       */
+/*   Updated: 2022/02/02 18:32:13 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef REVERSE_ITERATOR_HPP
 # define REVERSE_ITERATOR_HPP
+
+# include "iterator_traits.hpp"
 
 namespace ft
 {
@@ -19,7 +21,13 @@ namespace ft
 		class reverse_iterator
 		{
 			public:
-		}
+				typedef Iterator												iterator_type;
+				typedef typename iterator_traits<Iterator>::value_type			value_type;
+				typedef typename iterator_traits<Iterator>::difference_type		difference_type;
+				typedef typename iterator_traits<Iterator>::pointer				pointer;
+				typedef typename iterator_traits<Iterator>::reference			reference;
+				typedef typename iterator_traits<Iterator>::iterator_category	iterator_category;
+		};
 }
 
 #endif
