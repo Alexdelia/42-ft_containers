7118923c9dffbd0875dacb50035c7b688c971726
Alexdelia <alexandre.delille.57@gmail.com>
2022-02-06 13:52:21 +0100

    test reverse iterator

--- test/vector.cpp
+++ test/vector.cpp
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 11:20:48 by adelille          #+#    #+#             */
-/*   Updated: 2022/02/06 13:33:02 by adelille         ###   ########.fr       */
+/*   Updated: 2022/02/06 13:51:59 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ static int	test_constructor(void)
 	ft::vector<int>	v5(v3.begin(), v3.end());			// iterator
 	int				i[] = {42, 1, -84};
 	ft::vector<int>	v6(i, i + sizeof(i) / sizeof(int));	// iterator from array
-	ft::vector<int>	v7 = v6;
+	ft::vector<int>	v7 = v6;							// assign
 
 	std::cerr << "default:             " << vtos(v1) << std::endl;
 	std::cerr << "size:                " << vtos(v2) << std::endl;
@@ -63,9 +63,9 @@ static int	test_constructor_std(void)
 	std::vector<int>	v3(3, 42);							// size + value
 	std::vector<int>	v4(v3);								// copy
 	std::vector<int>	v5(v3.begin(), v3.end());			// iterator
-	int				i[] = {42, 1, -84};
+	int					i[] = {42, 1, -84};
 	std::vector<int>	v6(i, i + sizeof(i) / sizeof(int));	// iterator from array
-	std::vector<int>	v7 = v6;
+	std::vector<int>	v7 = v6;							// assign
 
 	std::cerr << "default:             " << vtos(v1) << std::endl;
 	std::cerr << "size:                " << vtos(v2) << std::endl;
@@ -78,12 +78,65 @@ static int	test_constructor_std(void)
 	return (false);
 }
 
+static int	test_v_iterator(void)
+{
+	ft::vector<int>	v1;
+	ft::vector<int>	v2(21, 42);
+
+	std::cerr << "|";
+	for (ft::reverse_iterator<ft::vector<int>::iterator > i = v1.rbegin();
+			i != v1.rend(); ++i)
+		std::cerr << *i << "|";
+	std::cerr << std::endl << "size: " << v1.size() << std::endl;
+	if (v1.size() != 0)
+		return (true);
+	
+	std::cerr << "|";
+	for (ft::reverse_iterator<ft::vector<int>::iterator > i = v2.rbegin();
+			i != v2.rend(); ++i)
+		std::cerr << *i << "|";
+	std::cerr << std::endl << "size: " << v2.size() << std::endl;
+	if (v1.size() != 21)
+		return (true);
+
+	return (false);
+}
+
+static int	test_v_iterator_std(void)
+{
+	std::vector<int>	v1;
+	std::vector<int>	v2(21, 42);
+
+	std::cerr << "|";
+	for (std::reverse_iterator<std::vector<int>::iterator > i = v1.rbegin();
+			i != v1.rend(); ++i)
+		std::cerr << *i << "|";
+	std::cerr << std::endl << "size: " << v1.size() << std::endl;
+	if (v1.size() != 0)
+		return (true);
+	
+	std::cerr << "|";
+	for (std::reverse_iterator<std::vector<int>::iterator > i = v2.rbegin();
+			i != v2.rend(); ++i)
+		std::cerr << *i << "|";
+	std::cerr << std::endl << "size: " << v2.size() << std::endl;
+	if (v1.size() != 21)
+		return (true);
+
+	return (false);
+}
+
 bool	test_vector(void)
 {
 	bool	ret;
 
 	ret = false;
 	ret |= tl("constructor", &test_constructor, &test_constructor_std);
+	ret |= tl("iterator", &test_v_iterator, &test_v_iterator_std);
+//	ret |= tl("capacity", &test_capacity, &test_capacity_std);
+//	ret |= tl("element access", &test_element access, &test_element access_std);
+//	ret |= tl("modifier", &test_modifier, &test_modifier_std);
 
+	return (true); //
 	return (ret);
 }
