2a2d83ebf75ce40b1a5b2f535d9ba48d6b90af67
Alexdelia <alexandre.delille.57@gmail.com>
2022-02-16 10:54:23 +0100

    save because need to code rbtree

--- src/map.hpp
+++ src/map.hpp
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 13:02:00 by adelille          #+#    #+#             */
-/*   Updated: 2022/02/16 10:27:03 by adelille         ###   ########.fr       */
+/*   Updated: 2022/02/16 10:54:07 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,40 @@
 
 # include "lexicographical_compare.hpp"
 # include "equal.hpp"
-//# include "pair.hpp"
+# include "pair.hpp"
 
-// https://www.cplusplus.com/reference/stack/stack/
+// https://www.cplusplus.com/reference/map/map/
 
 namespace ft
 {
-	template <
-		class Key,
-		class T,
-		class Compare = std::less<Key>,
-		class Allocator = std::allocator< ft::pair< Key, T> > >
-		class stack
-		{
-			public:
-				// typedef
+	template < class Key, class T, class Compare = std::less<Key>,
+			 class Allocator = std::allocator< ft::pair< Key, T> > >
+				 class map
+				 {
+					 public:
+						 typedef Key								key_type;
+						 typedef T									mapped_type;
+						 typedef ft::pair<key_type, mapped_type>	value_type;
+						 typedef std::size_t						size_type;
+						 typedef std::ptrdiff_t						difference_type;
+						 typedef Compare							key_compare;
+						 typedef Allocator							allocator_type;
+						 typedef value_type							&reference;
+						 typedef const value_type					&const_reference;
+						 typedef value_type							*pointer;
+						 typedef const value_type					*const_pointer;
 
-			protected:
-				//
+					 protected:
+						 allocator_type		_alloc;
 
-			public:
-				//
+					 public:
+						 explicit map(const key_compare &comp = key_compare(),
+								 const allocator_type &alloc = allocator_type())
+						 {
 
-		};
+						 }
+
+				 };
 }
 
 #endif
